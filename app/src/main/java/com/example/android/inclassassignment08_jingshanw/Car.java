869c00b2523e3d049eae46d3c9af035db3f7e9ef
jingshanW <jw3435@tc.columbia.edu>
2017-03-28 19:19:26 -0400

    add car class

---
+++ app/src/main/java/com/example/android/inclassassignment08_jingshanw/Car.java
@@ -0,0 +1,42 @@
+package com.example.android.inclassassignment08_jingshanw;
+
+/**
+ * Created by Jing on 3/28/17.
+ */
+
+public class Car
+{
+    private String brand;
+    private int price;
+    private boolean suv;
+
+    public Car(String brand, boolean suv, int price) {
+        this.brand = brand;
+        this.suv = suv;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isSuv() {
+        return suv;
+    }
+
+    public void setSuv(boolean suv) {
+        this.suv = suv;
+    }
+}
--- app/src/main/java/com/example/android/inclassassignment08_jingshanw/SecondActivity.java
+++ app/src/main/java/com/example/android/inclassassignment08_jingshanw/SecondActivity.java
@@ -4,14 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 
-    public void addd(View view) {
+    public void addToFirebase(View view) {
     }
 }
